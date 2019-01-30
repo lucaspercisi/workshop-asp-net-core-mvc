4c022cf48ea7d9f65b12f3561dfb74d1100968bc
Lucas <lucas_percisi@hotmail.com>
2019-01-29 23:26:24 -0200

    Deletando controller para fazer crud

--- SalesWebMVC/Models/Departament.cs
+++ SalesWebMVC/Models/Departament.cs
@@ -7,5 +7,6 @@ namespace SalesWebMVC.Models {
     public class Departament {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
