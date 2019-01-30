b2052feada5caeca4206ab84349600c3cbd75d5a
Lucas <lucas_percisi@hotmail.com>
2019-01-29 22:36:07 -0200

    Primeiro modelView

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers {
     public class HomeController : Controller {
@@ -14,6 +14,7 @@ namespace SalesWebMVC.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Email"] = "lucas_percisi@hotmail.com";
 
             return View();
         }
